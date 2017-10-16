bfa45828927cb5557dcbe621b75ad65b2aad39a2
Phuoc Le <lethienphuoc1991@gmail.com>
2017-10-16 12:56:31 +0700

    More ReStructuring

---
+++ FabulousStuff/Models/Config/UserConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FabulousStuff.Models.Config
+{
+    public class UserConfig
+    {
+    }
+
+    public enum UserLevel
+    {
+        SuperAdmin = 1,
+        Admin = 2,
+        Supervisor = 3,
+    }
+}
---
+++ FabulousStuff/Models/User/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FabulousStuff.Models.User
+{
+    public class User
+    {
+        public User()
+        {
+            DateCreated = DateTime.Now;
+        }
+
+        [BsonId]
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        private DateTime? DateCreated { get; }
+        private DateTime? LastLoginTime { get; set; }
+        private DateTime? LastActivityDate { get; set; }
+
+        // Level of User.
+        // Will be defined by another Function.
+        private byte Level { get; set; }
+    }
+}
